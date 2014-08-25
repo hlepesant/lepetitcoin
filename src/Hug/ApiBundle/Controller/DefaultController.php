78309eee05d269ff79eb656f05971d8a20b0bf45
Hugues Lepesant <hugues@lepesant.com>
2014-08-26 00:28:57 +0200

    index with elasticsearch-php

--- src/Hug/ApiBundle/Controller/DefaultController.php
+++ src/Hug/ApiBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace Hug\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Elasticsearch\Client;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('HugApiBundle:Default:index.html.twig', array('name' => $name));
+        $params = array(
+            'hosts' => array (
+                '127.0.0.1:9200',
+        ));
+
+        $client = new Client($params);
+
+        $getParams = array(
+            'index' => 'toilet',
+            'type'  => 'external',
+            'id'    => '1',
+        );
+        $retDoc = $client->get($getParams);
+
+        return new JsonResponse($retDoc);
     }
 }
